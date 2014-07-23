9336e1179ba0d20b7722f48668ff925c700385a9
Kim, In Gee <ingee@paran.com>
2014-07-23 22:40:32 +0900

    make test-code for xml-parsing

---
+++ freemind-webapp-project-memo.mm
@@ -0,0 +1,25 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1403439643166" ID="ID_241255190" MODIFIED="1403439653314" TEXT="FreeMind WebApp">
+<node CREATED="1403439654992" ID="ID_1289525172" MODIFIED="1403439657596" POSITION="right" TEXT="xml parsing">
+<node CREATED="1403439664909" ID="ID_1997794936" MODIFIED="1403439670244" TEXT="stack-overflow">
+<node CREATED="1403439671366" ID="ID_558809868" MODIFIED="1403439672373" TEXT="http://stackoverflow.com/questions/7949752/cross-browser-javascript-xml-parsing"/>
+<node CREATED="1403439692571" ID="ID_664664443" MODIFIED="1403439695751" TEXT="Q: Whats the easiest cross-browser/cross-platform way to parse XML files in Javascript?"/>
+<node CREATED="1403439696353" ID="ID_1964208594" MODIFIED="1403439746209" TEXT="A: use jQuery.parseXML()">
+<node CREATED="1404309302700" ID="ID_1073174871" MODIFIED="1406122707096" TEXT="http://api.jquery.com/jquery.parsexml/">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+<node CREATED="1403439727372" ID="ID_1994648598" MODIFIED="1403439759569" TEXT="A: use SAX way">
+<node CREATED="1403439760687" ID="ID_994691326" MODIFIED="1403439761425" TEXT="https://github.com/isaacs/sax-js/"/>
+</node>
+</node>
+<node CREATED="1406122738746" ID="ID_433305336" MODIFIED="1406122744703" TEXT="todo">
+<node CREATED="1406122711282" ID="ID_1042150107" MODIFIED="1406122735802" TEXT="parse xml using jQuery.parseXML()"/>
+<node CREATED="1406122747117" ID="ID_534903524" MODIFIED="1406122778370" TEXT="traverse xml-DOM">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+</node>
+</node>
+</map>
---
+++ sample-data/new-map.mm
@@ -0,0 +1,4 @@
+﻿<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1393257871227" ID="ID_503560790" MODIFIED="1393257880435" TEXT="new-map"/>
+</map>
