b560686a1e5b5de6283f5349e1946c1c554a21ed
Kim, In Gee <ingee@paran.com>
2014-12-16 11:59:31 +0900

    AF UI test : test with generator-backbone
    - scaffold project with generator-backbone
    - bower install intel-appframework

---
+++ test-code/ui/afui-test/README.txt
@@ -0,0 +1,5 @@
+AF UI test project
+
+* test AF UI
+* test with generator-backbone
+
---
+++ test-code/ui/afui-test/app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global AfuiTest, $*/
+
+
+window.AfuiTest = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    AfuiTest.init();
+});
