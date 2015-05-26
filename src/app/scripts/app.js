838d4e145c4a541bddaba5d7b0b039739c283c83
InGee Kim <ingee@paran.com>
2015-05-26 23:13:49 +0900

    generating node-view-tree from hard-coded XML
      - test MindNode type's feasibility with JSON before XML (trying...)

--- src/app/scripts/app.js
+++ src/app/scripts/app.js
@@ -4,7 +4,8 @@ var $ = $ || {};
 var Backbone = Backbone || {};
 
 var App = {
-  sampleMindMapData: '<map version="0.9.0">' +
+  sampleMindMapData: 
+    '<map version="0.9.0">' +
     '<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->' +
     '<node CREATED="3" ID="1" MODIFIED="2" TEXT="new-map">' +
     '<node CREATED="1406119930596" ID="ID_1238146207" MODIFIED="1406119935806" POSITION="right" TEXT="this is A"/>' +
@@ -16,6 +17,20 @@ var App = {
     '<node CREATED="1406119939270" ID="ID_442146008" MODIFIED="1406119942491" POSITION="right" TEXT="&#xc774;&#xac83;&#xc740; C"/>' +
     '</node>' +
     '</map>', 
+  sampleMindMapJson: 
+    '{ "text": "new-map",' +
+      '"childNodes":' +
+        '[ { "text": "this is A" },' +
+          '{ "text": "this is B",' +
+            '"childNodes":' +
+              '[ { "text": "Sub-Sub-1" },' +
+                '{ "text": "Sub-Sub-2" },' +
+                '{ "text": "Hello, World?" }' +
+              ']' +
+          '},' +
+          '{ "text": "this is C" }' +
+        ']' +
+    '}',
 
   start: function() {
     console.log('app.start()');
